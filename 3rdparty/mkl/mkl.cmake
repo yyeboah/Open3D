1da55644d857337c46f4b16ee81f8e4dd71de2ab
Yixing Lao <yixing.lao@gmail.com>
2020-07-09 18:18:06 -0700

    disable build and install command

--- 3rdparty/mkl/mkl.cmake
+++ 3rdparty/mkl/mkl.cmake
@@ -3,7 +3,10 @@ include(ExternalProject)
 set(MKL_URL_LINUX "https://anaconda.org/anaconda/mkl/2020.1/download/linux-64/mkl-2020.1-217.tar.bz2")
 
 ExternalProject_Add(ext_mkl
+    PREFIX mkl
     URL ${MKL_URL_LINUX}
     CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
     EXCLUDE_FROM_ALL TRUE
 )

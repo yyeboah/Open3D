e68bdd78475167b3c8f172afff0d710100abff70
Yixing Lao <yixing.lao@gmail.com>
2020-07-20 21:56:52 -0700

    end-to-end working

--- openblas.cmake
+++ openblas.cmake
@@ -1,18 +1,22 @@
 include(ExternalProject)
 
+set(OPENBLAS_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/openblas)
+
 ExternalProject_Add(
     ext_openblas
     PREFIX openblas
     GIT_REPOSITORY https://github.com/xianyi/OpenBLAS.git
     GIT_TAG v0.3.10
-    INSTALL_COMMAND "" # Install command not compatible with powershell
-    CMAKE_ARGS -DMSVC_STATIC_CRT=${STATIC_WINDOWS_RUNTIME}
+    # INSTALL_COMMAND "" # Install command not compatible with powershell
+    CMAKE_ARGS
+        -DMSVC_STATIC_CRT=${STATIC_WINDOWS_RUNTIME}
+        -DCMAKE_INSTALL_PREFIX=${OPENBLAS_INSTALL_PREFIX}
 )
 
-ExternalProject_Get_property(ext_openblas SOURCE_DIR)
-set(OPENBLAS_INCLUDE_DIR "${SOURCE_DIR}/") # The "/"" is critical, see import_3rdparty_library.
-set(OPENBLAS_LIB_DIR "${SOURCE_DIR}")
+set(OPENBLAS_INCLUDE_DIR "${OPENBLAS_INSTALL_PREFIX}/include/openblas")
+set(OPENBLAS_LIB_DIR "${OPENBLAS_INSTALL_PREFIX}/lib")
 set(OPENBLAS_LIBRARIES openblas)
+
 message(STATUS "OPENBLAS_INCLUDE_DIR: ${OPENBLAS_INCLUDE_DIR}")
 message(STATUS "OPENBLAS_LIB_DIR ${OPENBLAS_LIB_DIR}")
 message(STATUS "OPENBLAS_LIBRARIES: ${OPENBLAS_LIBRARIES}")

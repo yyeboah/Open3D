3c7eb296ac2e8564aecc1a0df21cc845600f5eb2
Yixing Lao <yixing.lao@gmail.com>
2020-07-20 19:05:31 -0700

    run script

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 
-#include <cblas.h>
+// #include <cblas.h>
 #include <iostream>
 
-void DummyOpenBlasTest() {
-    int i = 0;
-    double A[6] = {1.0, 2.0, 1.0, -3.0, 4.0, -1.0};
-    double B[6] = {1.0, 2.0, 1.0, -3.0, 4.0, -1.0};
-    double C[9] = {.5, .5, .5, .5, .5, .5, .5, .5, .5};
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, 3, 3, 2, 1, A, 3, B, 3,
-                2, C, 3);
-    for (i = 0; i < 9; i++) {
-        std::cout << C[i] << " ";
-    }
-    std::cout << std::endl;
-}
+// void DummyOpenBlasTest() {
+//     int i = 0;
+//     double A[6] = {1.0, 2.0, 1.0, -3.0, 4.0, -1.0};
+//     double B[6] = {1.0, 2.0, 1.0, -3.0, 4.0, -1.0};
+//     double C[9] = {.5, .5, .5, .5, .5, .5, .5, .5, .5};
+//     cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, 3, 3, 2, 1, A, 3, B,
+//     3,
+//                 2, C, 3);
+//     for (i = 0; i < 9; i++) {
+//         std::cout << C[i] << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
 int main() {
-    std::cout << "before" << std::endl;
-    DummyOpenBlasTest();
-    std::cout << "after" << std::endl;
+    std::cout << "main: before" << std::endl;
+    // DummyOpenBlasTest();
+    std::cout << "main: after" << std::endl;
     return 0;
 }
---
+++ run.ps1
@@ -0,0 +1,12 @@
+Write-Output "To run config"
+build
+cmake -G "Visual Studio 16 2019" -A x64 ..
+Write-Output "Config done"
+
+Write-Output "To run build"
+cmake --build . --config Release --target ALL_BUILD
+Write-Output "Build done"
+
+Write-Output "To run exe"
+.\Release\blas_demo.exe
+Write-Output "Run exe done"

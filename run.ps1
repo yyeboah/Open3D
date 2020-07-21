48585442d0b5f8817b8232a0fa60d62d9df66790
Yixing Lao <yixing.lao@gmail.com>
2020-07-20 19:15:39 -0700

    first running

--- run.ps1
+++ run.ps1
@@ -1,10 +1,12 @@
+$ErrorActionPreference = "Stop"
+
 Write-Output "To run config"
 build
 cmake -G "Visual Studio 16 2019" -A x64 ..
 Write-Output "Config done"
 
 Write-Output "To run build"
-cmake --build . --config Release --target ALL_BUILD
+cmake --build . --verbose --config Release --target ALL_BUILD
 Write-Output "Build done"
 
 Write-Output "To run exe"

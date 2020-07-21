3b75a89b1edfb191f8af28146b642570f4da5b89
Yixing Lao <yixing.lao@gmail.com>
2020-07-20 21:00:35 -0700

    build ok

---
+++ openblas.cmake
@@ -0,0 +1,17 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+    ext_openblas
+    PREFIX openblas
+    GIT_REPOSITORY https://github.com/xianyi/OpenBLAS.git
+    GIT_TAG v0.3.10
+    INSTALL_COMMAND "" # Install command not compatible with powershell
+)
+
+ExternalProject_Get_property(ext_openblas SOURCE_DIR)
+set(OPENBLAS_INCLUDE_DIR "${SOURCE_DIR}/") # The "/"" is critical, see import_3rdparty_library.
+set(OPENBLAS_LIB_DIR "${SOURCE_DIR}")
+set(OPENBLAS_LIBRARIES openblas)
+message(STATUS "OPENBLAS_INCLUDE_DIR: ${OPENBLAS_INCLUDE_DIR}")
+message(STATUS "OPENBLAS_LIB_DIR ${OPENBLAS_LIB_DIR}")
+message(STATUS "OPENBLAS_LIBRARIES: ${OPENBLAS_LIBRARIES}")
